8ac492b50e0406de18163a7c68ef12c6c57e038e
Alexander Kulagin <>
2020-02-06 11:29:08 +0200

    test commit 1

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: A UIKit based Playground for presenting user interface
+  
+import UIKit
+import PlaygroundSupport
+
+
+let a = "    asa    "
+let b = a.trimmingCharacters(in: .whitespacesAndNewlines)
+print(b)
